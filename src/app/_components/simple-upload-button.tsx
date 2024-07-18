6820e8bc72e3e187256dd653d00d4e3eb4cc9f9e
Dimitar <dimitar.mandarliev@gmail.com>
2024-07-18 11:52:16 +0300

    ratelimit implemented

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -88,6 +88,11 @@ export function SimpleUploadButton() {
         },
       );
     },
+    onUploadError(e) {
+      posthog.capture("upload-error", { e });
+      toast.dismiss("upload-begin");
+      toast.error("Upload failed");
+    },
     onClientUploadComplete() {
       toast.dismiss("upload-begin");
       toast("Upload complete!");
